53cf55edbd3ccb80a528043e928897709d5315ea
wvxbs <gabriel.ferreira7854@gmail.com>
2024-06-28 19:21:15 -0300

    index.py updated

--- src/index.py
+++ src/index.py
@@ -1,15 +1,7 @@
 from pokemon import Pokemon
 
-def printData(data):
-    for i in data:
-        print(i)
-
 def main():
-    i = 0
-    while i in range(3):
-        base_url = "https://pokeapi.co/api/v2/pokemon"
-        p = Pokemon(base_url, "/app/cache", 3600)
+    base_url = "https://pokeapi.co/api/v2/pokemon"
+    Pokemon(base_url, "/app/cache", 3600)
 
-        printData(p.getAllPokemon())
-        i += 1
 main()
